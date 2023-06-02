e7d54689a25b5e42c723ad93f390c45f7f606d96
SakinahNuruSyifa <113828555+SakinahNuruSyifa@users.noreply.github.com>
2023-06-02 22:46:31 +0800

    fix: button stage 3

--- app/src/main/java/elite/project/AddTugas.java
+++ app/src/main/java/elite/project/AddTugas.java
@@ -26,6 +26,7 @@ public class AddTugas extends Stage {
         Image statusBar = new Image(getClass().getResourceAsStream("/img/StatusBar.png"));
         ImageView viewStatusBar = new ImageView(statusBar);
         VBox vBox = new VBox(viewStatusBar);
+        vBox.setAlignment(Pos.TOP_CENTER);
         vBox.setStyle("-fx-background-color: rgb(44, 48, 57)");
 
         // label untuk inputfield judul tugas
@@ -75,27 +76,53 @@ public class AddTugas extends Stage {
         TextField deadlineField = new TextField();
         deadlineField.setStyle(
                 "-fx-background-color: black; -fx-background-radius: 12px; -fx-text-fill: white; -fx-background-insets: 0 0 -12 0; -fx-alignment: center-left; -fx-font-family: 'Poppins'; -fx-font-size: 18px; -fx-font-weight: 600;");
-        deadlineField.setAlignment(Pos.CENTER);
+        deadlineField.setAlignment(Pos.CENTER_LEFT);
         deadlineField.setFont(Font.loadFont(getClass().getResourceAsStream("/Poppins-Regular.ttf"), 16));
 
-        VBox inputContainer = new VBox();
-        inputContainer.setSpacing(12);
+        VBox inputContainer1 = new VBox(judulTugas, juduField);
+        inputContainer1.setSpacing(10);
+        inputContainer1.setAlignment(Pos.CENTER_LEFT);
+
+        VBox inputContainer2 = new VBox(tag, tagField);
+        inputContainer2.setSpacing(10);
+        inputContainer2.setAlignment(Pos.CENTER_LEFT);
+
+        VBox inputContainer3 = new VBox(label, labelField);
+        inputContainer3.setSpacing(10);
+        inputContainer3.setAlignment(Pos.CENTER_LEFT);
+
+        VBox inputContainer4 = new VBox(deadline, deadlineField);
+        inputContainer4.setSpacing(10);
+        inputContainer4.setAlignment(Pos.CENTER_LEFT);
+
+        VBox inputContainer = new VBox(inputContainer1, inputContainer2, inputContainer3, inputContainer4);
+        inputContainer.setSpacing(50);
+        inputContainer.setAlignment(Pos.CENTER);
+        inputContainer.setPadding(new Insets(0, 15, 0, 15));
 
         // Button Simpan
         Button simpaButton = new Button("Simpan");
-        simpaButton.setPrefWidth(258);
+        simpaButton.setPrefWidth(350);
         simpaButton.setPrefHeight(55);
         simpaButton.setLineSpacing(25);
+        simpaButton.setAlignment(Pos.CENTER);
+        // simpaButton.setStyle(new Insets(200, 0, 0, 0));
         simpaButton.setFont(Font.loadFont(getClass().getResourceAsStream("/Poppins-Medium.ttf"), 24));
         simpaButton.setStyle(
-                "-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-background-radius: 16px; -fx-alignment: TOP_CENTER;");
+                "-fx-background-color: #6773E9; -fx-text-fill: white; -fx-background-radius: 16px; -fx-alignment: TOP_CENTER;");
 
         // Vbox
-        VBox vLayout = new VBox(vBox, judulTugas, juduField, tag, tagField, label, labelField, deadline, deadlineField,
-                simpaButton);
+        VBox layoutButton = new VBox(simpaButton);
+        vBox.getChildren().add(layoutButton);
+        layoutButton.setAlignment(Pos.CENTER);
+
+
+        VBox vLayout = new VBox(vBox, inputContainer ,layoutButton);
         vLayout.setStyle("-fx-background-color: #222831");
-        vLayout.setAlignment(Pos.TOP_CENTER);
-        vLayout.setSpacing(15);
+        vLayout.setAlignment(Pos.CENTER);
+        vLayout.setSpacing(100);
+
+
         // Menampilkan konten di dalam stage
         Scene scene = new Scene(vLayout, 428, 999);
         setResizable(false);
