e1fae0737cdbf13ab52e26efcaffe7f211fef6d3
zoelramadhan <zoelramadhan123@gmail.com>
2023-06-03 13:51:46 +0800

    Add: Logic Level & Exp

--- app/src/main/java/elite/project/Home.java
+++ app/src/main/java/elite/project/Home.java
@@ -8,7 +8,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Home extends Stage {
@@ -16,6 +19,9 @@ public class Home extends Stage {
     private Button selectedCharacter;
     private StackPane root;
     private Stage primaryStage;
+    private int exp = 0;
+    private int level = 1;
+    private int expRequired = 30;
 
     public Home(Button submitButton) {
         this.selectedCharacter = submitButton;
@@ -38,33 +44,61 @@ public class Home extends Stage {
         addTask.setStyle(
             "-fx-background-color: #6773E9; -fx-text-fill: white; -fx-background-radius: 16px;");
 
-        addTask.setOnAction(event -> {
-            AddTugas addTugas = new AddTugas();
-            addTugas.show();
-            primaryStage.close();
-        });
+            addTask.setOnAction(event -> {
+                AddTugas addTugas = new AddTugas();
+                addTugas.show();
+                primaryStage.close();
+            });
+            
+            
+            
+        // Menambahkan level dan exp
+        Text levelText = new Text("Level: " + level + " ");
+        levelText.setFont(Font.loadFont(getClass().getResourceAsStream("/Poppins-Medium.ttf"), 18));
+        levelText.setFill(Color.WHITE);
+                    
+        Text expText = new Text("Exp: " + exp + "/" + expRequired);
+        expText.setFont(Font.loadFont(getClass().getResourceAsStream("/Poppins-Medium.ttf"), 18));
+        expText.setFill(Color.WHITE);
+
+        HBox milestone = new HBox();
+        milestone.getChildren().addAll(levelText, expText);
+        milestone.setPadding(new Insets(0, 0, 770, 0));
+        milestone.setAlignment(Pos.CENTER);
+        
+        VBox box1 = new VBox();
+        box1.setAlignment(Pos.CENTER);
+        box1.getChildren().addAll(milestone);
 
         
-        HBox box1 = new HBox();
-        box1.setPadding(new Insets(300, 20, 0, 20));
-        box1.setAlignment(Pos.CENTER);
-        box1.getChildren().addAll(addTask);
-
         // Membuat ImageView untuk menampilkan gambar
         Image backgroundImage = new Image(getClass().getResourceAsStream("/img/HomeEmpty.png"));
         ImageView backgroundImageView = new ImageView(backgroundImage);
-        root.getChildren().addAll(backgroundImageView, box1);
-
+        
         // Menambahkan gambar karakter yang dipilih
         setCharacterImage();
+        
+        VBox showbox1 = new VBox();
+        showbox1.setAlignment(Pos.CENTER);
+        showbox1.getChildren().addAll(box1);
 
-
+        root.getChildren().addAll(backgroundImageView, showbox1, addTask);
+        
         HBox vLayout = new HBox(root);
+        
         // Menampilkan konten di dalam stage
         Scene scene = new Scene(vLayout, 428, 800);
         setResizable(false);
         setScene(scene);
+        
+        // HBox infoBox = new HBox(levelText, expText);
+        // infoBox.setAlignment(Pos.CENTER_LEFT);
+        // infoBox.setSpacing(10);
+        // infoBox.setPadding(new Insets(10));
+        
+        // // root.getChildren().add(infoBox);
     }
+    
 
     private void setCharacterImage() {
         String imagePath = null;
